8c90125f5b7bde088adf9f0c5a91783a5fda3413
Max <rekin.m@gmail.com>
2023-11-28 23:22:00 +0300

    Fixes

--- firmware/variants/diy/diy_nano_c3/variant.h
+++ firmware/variants/diy/diy_nano_c3/variant.h
@@ -1,8 +1,8 @@
 #define I2C_SDA 8
 #define I2C_SCL 9
 
-#define BUTTON_PIN 0 // button
-#define BUTTON_NEED_PULLUP
+//#define BUTTON_PIN  // button
+//#define BUTTON_NEED_PULLUP
 
 //#define HAS_SCREEN 0
 #define HAS_GPS 0
@@ -15,11 +15,11 @@
 #undef RF95_NSS
 
 
-#define LORA_DIO0    // a No connect on the SX1262/SX1268 module
+#define LORA_DIO0 RADIOLIB_NC   // a No connect on the SX1262/SX1268 module
 #define LORA_RESET 3 // RST for SX1276, and for SX1262/SX1268
-#define LORA_DIO1 4  // IRQ for SX1262/SX1268
+#define LORA_DIO1 0  // IRQ for SX1262/SX1268
 #define LORA_DIO2 10  // BUSY for SX1262/SX1268
-#define LORA_DIO3     // Not connected on PCB, but internally on the TTGO SX1262/SX1268, if DIO3 is high the TXCO is enabled
+#define LORA_DIO3 RADIOLIB_NC    // Not connected on PCB, but internally on the TTGO SX1262/SX1268, if DIO3 is high the TXCO is enabled
 
 #define RF95_SCK 4
 #define RF95_MISO 5
@@ -40,6 +40,8 @@
 //#define SX126X_RXEN RADIOLIB_NC // Defining the RXEN ruins RFSwitching for the E22 900M30S in RadioLib
 #define SX126X_RXEN 2 // Defining the RXEN ruins RFSwitching for the E22 900M30S in RadioLib
 #define SX126X_TXEN 1
+
+#define SX126X_DIO3_TCXO_VOLTAGE 1.8
 
 // Adafruit RFM95W OK
 // https://www.adafruit.com/product/3072
